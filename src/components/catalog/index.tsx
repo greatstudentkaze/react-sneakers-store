d68bf466519c3d71892863c6ff264717b1aa722e
Ivan Nefagin <ivan.nefagin@fuse8.ru>
2021-06-19 23:50:36 +0300

    feat: render CardItem's on Catalog

--- src/components/catalog/index.tsx
+++ src/components/catalog/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import CardItem from '../card-item';
 import './styles/catalog.scss';
 import searchIconSrc from '../../assets/images/icon/search.svg';
 
 interface Props {
   title: string
 }
+
+const catalog = [
+  {
+    id: '123',
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    currency: 'RUB',
+    imageSrc: {
+      '1x': 'catalog/sneakers/nike-blazer-mid-suede.jpg',
+      '2x': 'catalog/sneakers/nike-blazer-mid-suede@2x.jpg',
+    },
+  },
+  {
+    id: '1234',
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    currency: 'RUB',
+    imageSrc: {
+      '1x': 'catalog/sneakers/nike-blazer-mid-suede.jpg',
+      '2x': 'catalog/sneakers/nike-blazer-mid-suede@2x.jpg',
+    },
+  },
+  {
+    id: '12345',
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    currency: 'RUB',
+    imageSrc: {
+      '1x': 'catalog/sneakers/nike-blazer-mid-suede.jpg',
+      '2x': 'catalog/sneakers/nike-blazer-mid-suede@2x.jpg',
+    },
+  },
+];
 
 const Catalog = ({ title }: Props) => {
   return (
@@ -16,7 +50,15 @@ const Catalog = ({ title }: Props) => {
         <input className="catalog__search" type="text" id="catalog-search" placeholder="Поиск..." style={{ backgroundImage: `url("${searchIconSrc}")` }} />
       </header>
       <ul className="catalog__list">
-        <li></li>
+        {
+          !catalog.length
+            ? ': - ('
+            : catalog.map((item, i) => <li key={item.id}>
+              <CardItem title={item.title} imageSrc={item.imageSrc} index={i} price={item.price} currency={item.currency} />
+            </li>)
+        }
+        <li>
+        </li>
       </ul>
     </section>
   );
