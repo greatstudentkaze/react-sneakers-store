cf47d8733beb856a33612841fd4a09fa7c977921
Ivan Nefagin <ivan.nefagin@fuse8.ru>
2021-06-19 21:23:11 +0300

    feat: add Header component and base styles

---
+++ src/components/header/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './styles/header.scss';
+import logoSrc from '../../assets/images/logo.svg';
+import cartIconSrc from '../../assets/images/icon/cart.svg';
+import wishlistIconSrc from '../../assets/images/icon/wishlist-nav.svg';
+import loginIconSrc from '../../assets/images/icon/login.svg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <a className="logo" href="/">
+          <img src={logoSrc} width="245" height="41" alt="Логотип React Sneakers" />
+        </a>
+        <nav>
+          <ul className="user-list">
+            <li className="user-list__item">
+              <a href="/" style={{ backgroundImage: `url("${cartIconSrc}")` }}>
+                <span className="visually-hidden">Корзина:</span>
+                <span>1205 руб.</span>
+              </a>
+            </li>
+            <li className="user-list__item user-list__item--icon">
+              <a href="/" style={{ backgroundImage: `url("${wishlistIconSrc}")` }}>
+                <span className="visually-hidden">Список желаний</span>
+              </a>
+            </li>
+            <li className="user-list__item user-list__item--icon">
+              <a href="/" style={{ backgroundImage: `url("${loginIconSrc}")` }}>
+                <span className="visually-hidden">Вход</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
