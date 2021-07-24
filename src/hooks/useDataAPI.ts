0e69849d99186d97d348ec2cc19389adc3f82d58
greatstudentkaze <dfyz0r@list.ru>
2021-07-24 10:52:46 +0300

    feat(hooks/useDataAPI): add dataFetchReducer

--- src/hooks/useDataAPI.ts
+++ src/hooks/useDataAPI.ts
@@ -1,33 +1,66 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import axios from 'axios';
+
+interface State {
+  isLoading: boolean,
+  isError: boolean,
+  data: any,
+}
+
+export type DataFetchAction = { type: 'FETCH_INIT' } | { type: 'FETCH_SUCCESS', payload: State['data'] } | { type: 'FETCH_FAILURE' };
+
+const dataFetchReducer = (state: State, action: DataFetchAction): State => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      return state;
+  }
+};
 
 const useDataAPI = <T>(initialURL: string, initialData: T) => {
   const [url, setURL] = useState(initialURL);
-  const [data, setData] = useState(initialData);
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
+      dispatch({ type: 'FETCH_INIT' });
 
       try {
         const { data } = await axios.get<typeof initialData>(url);
 
-        setData(data);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (err) {
-        setIsError(true);
+        dispatch({ type: 'FETCH_FAILURE' });
       }
-
-      setIsLoading(false);
     };
 
     fetchData();
   }, [url]);
 
-  return [{ data, isLoading, isError }, setURL] as const;
+  return [state, setURL] as const;
 };
 
 export default useDataAPI;
