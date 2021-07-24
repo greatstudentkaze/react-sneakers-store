7120b96301491786dd885f66ef9033c1cd49fe49
greatstudentkaze <dfyz0r@list.ru>
2021-07-24 14:56:12 +0300

    feat: add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './styles/index.scss';
 import App from './components/app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
