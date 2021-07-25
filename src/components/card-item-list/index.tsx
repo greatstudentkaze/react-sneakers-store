1569e75408a5d00b2531880edbea65f5970c24f3
greatstudentkaze <dfyz0r@list.ru>
2021-07-25 14:04:38 +0300

    feat(components): add CardItemList component to render CardItem's

---
+++ src/components/card-item-list/index.tsx
@@ -0,0 +1,53 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import cn from 'classnames';
+
+import { SneakersItem } from '../../interfaces/sneakers.interface';
+
+import CardItem from '../card-item';
+import CardItemSkeleton from '../card-item/skeleton';
+
+import './styles/card-item-list.scss';
+
+interface CardItemListProps extends DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement> {
+  items: SneakersItem[],
+  isLoading: boolean,
+}
+
+const renderItems = (
+  items: SneakersItem[],
+  isLoading: boolean,
+) => {
+
+  if (items.length > 0) {
+    return items.map((item, i) => <li key={item.id}>
+      <CardItem
+        title={item.title}
+        imageSrc={item.imageSrc}
+        index={i}
+        price={item.price}
+        currency={item.currency}
+        id={item.id}
+      />
+    </li>);
+  }
+
+  if (isLoading) {
+    const skeletonItems = new Array(12).fill(true);
+
+    return skeletonItems.map((_, i) => <li key={i}>
+      <CardItemSkeleton />
+    </li>);
+  }
+
+  return 'Товары не найдены 😓';
+};
+
+const CardItemList = ({ items, isLoading, className, ...props }: CardItemListProps) => {
+  return (
+    <ul className={cn('card-item-list', className)} {...props}>
+      {renderItems(items, isLoading)}
+    </ul>
+  );
+};
+
+export default CardItemList;
