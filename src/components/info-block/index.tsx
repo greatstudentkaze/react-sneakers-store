72b9be884d481de509a1f06ed4b6f1de6bd44751
greatstudentkaze <dfyz0r@list.ru>
2021-07-25 21:07:01 +0300

    feat(components): add InfoBlock component

---
+++ src/components/info-block/index.tsx
@@ -0,0 +1,35 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+import './styles/info-block.scss';
+
+interface InfoBlockProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  title: string,
+  description: string,
+  imageData: {
+    src: string,
+    width: number,
+    height: number,
+  },
+  buttonText?: string,
+  onButtonClick: () => void,
+}
+
+const InfoBlock = ({ title, description, imageData, buttonText = 'Вернуться назад', onButtonClick }: InfoBlockProps) => {
+  return (
+    <div className="info-block">
+      <img className="info-block__image" src={imageData.src} width={imageData.width} height={imageData.height} alt="" />
+      <p className="info-block__title">{title}</p>
+      <p className="info-block__info">{description}</p>
+
+      <button className="info-block__button cart-panel__button cart-panel__button--arrow-left" type="button" onClick={onButtonClick}>
+        <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path d="M14.7144 7L1.00007 7" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M7 13L1 7L7 1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default InfoBlock;
