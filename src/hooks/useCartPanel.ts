9ad7b6adb1049d38629abf3831e240fcc6cea99a
greatstudentkaze <dfyz0r@list.ru>
2021-07-27 19:35:02 +0300

    feat: add custom scrollbar

--- src/hooks/useCartPanel.ts
+++ src/hooks/useCartPanel.ts
@@ -1,13 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const useCartPanel = (initialCartPanelState: boolean) => {
   const [isCartPanelOpened, setIsCatPanelOpened] = useState(initialCartPanelState);
-
-  useEffect(() => {
-    isCartPanelOpened
-      ? document.body.classList.add('no-scroll')
-      : document.body.classList.remove('no-scroll');
-  }, [isCartPanelOpened]);
 
   const openCartPanel = () => {
     setIsCatPanelOpened(true);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import './styles/index.scss';
 import App from './components/app';
@@ -8,7 +9,14 @@ import App from './components/app';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Scrollbars
+        style={{ height: '100vh' }}
+        autoHide
+        autoHideTimeout={5000}
+        autoHideDuration={300}
+      >
+        <App />
+      </Scrollbars>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
