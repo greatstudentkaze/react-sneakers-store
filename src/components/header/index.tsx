50addeb74273967378c7633b79789bfbb7776233
greatstudentkaze <dfyz0r@list.ru>
2021-07-30 00:51:14 +0300

    feat(header): add profile popup

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
-import { SyntheticEvent, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { SyntheticEvent, useContext, useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
+import { UserContext } from '../../context/user.context';
 import { AppContext } from '../../context/app.context';
 import { AppRoute } from '../../const';
 import { formatRuPrice } from '../../utils/formatRuPrice';
@@ -15,8 +16,25 @@ interface HeaderProps {
   openCartPanel: () => void,
 }
 
+const AuthButtonText = {
+  LOGIN: 'Войти',
+  LOGOUT: 'Выйти',
+}
+
 const Header = ({ openCartPanel }: HeaderProps) => {
+  const { user, logout, login } = useContext(UserContext);
   const { totalPrice } = useContext(AppContext);
+  const [authButtonText, setAuthButtonText] = useState(AuthButtonText.LOGIN);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    setAuthButtonText(user ? AuthButtonText.LOGOUT : AuthButtonText.LOGIN);
+  }, [user]);
+
+  const handleAuthButtonClick = () => {
+    user ? logout() : login({ email: 'hi@nefagin.ru' });
+  };
 
   const handleCartButtonClick = (evt: SyntheticEvent) => {
     evt.preventDefault();
@@ -43,8 +61,16 @@ const Header = ({ openCartPanel }: HeaderProps) => {
               </Link>
             </li>
             <li className="user-list__item user-list__item--icon">
-              <Link to={AppRoute.PROFILE} style={{ backgroundImage: `url("${loginIconSrc}")` }}>
+              <Link className="with-popup" to={AppRoute.PROFILE} style={{ backgroundImage: `url("${loginIconSrc}")` }}>
                 <span className="visually-hidden">Вход</span>
+                <div className="user-list__popup">
+                  <button type="button" onClick={() => history.push(AppRoute.PROFILE)}>
+                    Мой профиль
+                  </button>
+                  <button type="button" onClick={handleAuthButtonClick}>
+                    {authButtonText}
+                  </button>
+                </div>
               </Link>
             </li>
           </ul>
