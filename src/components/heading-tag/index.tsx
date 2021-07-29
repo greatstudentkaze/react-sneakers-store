8388ab25f6337a3d51070d62ae51479d5e2406e7
greatstudentkaze <dfyz0r@list.ru>
2021-07-30 00:00:16 +0300

    feat(heading-tag): import styles

--- src/components/heading-tag/index.tsx
+++ src/components/heading-tag/index.tsx
@@ -1,6 +1,8 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 import cn from 'classnames';
 
+import styles from './styles/heading-tag.module.scss';
+
 interface HeadingTagProps extends DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
   level: '1' | '2' | '3',
   children: ReactNode,
@@ -9,11 +11,11 @@ interface HeadingTagProps extends DetailedHTMLProps<HTMLAttributes<HTMLHeadingEl
 const HeadingTag = ({ level, children, className, ...props }: HeadingTagProps) => {
   switch (level) {
     case '1':
-      return <h1 className={cn(className)} {...props}>{children}</h1>
+      return <h1 className={cn(className, styles.h1)} {...props}>{children}</h1>
     case '2':
-      return <h2 className={cn(className)} {...props}>{children}</h2>
+      return <h2 className={cn(className, styles.h2)} {...props}>{children}</h2>
     case '3':
-      return <h3 className={cn(className)} {...props}>{children}</h3>
+      return <h3 className={cn(className, styles.h3)} {...props}>{children}</h3>
     default:
       return <></>;
   }
